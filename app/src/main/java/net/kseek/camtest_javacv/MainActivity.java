3f20fbba4df6ef1969f8a4f53e0acbf3c4600dd4
Teocci <jrfdeft@gmail.com>
2016-06-03 19:14:44 +0900

    first commit

---
+++ app/src/main/java/net/kseek/camtest_javacv/MainActivity.java
@@ -0,0 +1,24 @@
+package net.kseek.camtest_javacv;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import net.kseek.camtest_javacv.record.RecordActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.btnRecord).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RecordActivity.class));
+            }
+        });
+    }
+}
